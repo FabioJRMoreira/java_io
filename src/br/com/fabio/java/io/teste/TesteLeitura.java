a7424584082bdd1926d6c859bceaf84df90aa7a3
FabioJRMoreira <tulkas.fabio@hotmail.com>
2022-07-14 14:05:04 -0400

    commit classe como metod de leitura de arquivo. stream usado para arquivos binarios img pdf reader usados para aquivos texto

--- src/br/com/fabio/java/io/teste/TesteLeitura.java
+++ src/br/com/fabio/java/io/teste/TesteLeitura.java
@@ -1,5 +1,35 @@
 package br.com.fabio.java.io.teste;
 
-public class TesteLeitura {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class TesteLeitura {
+	public static void main(String[] args) {
+		
+		try {
+			
+			//padrao de projeto decorator
+			//Fluxo de Entrada com arquivo
+			FileInputStream fis = new FileInputStream("lorem.txt");//binario
+			//ler um InputStream
+			InputStreamReader isr = new InputStreamReader(fis);//trasforma os bytes em caracteres
+			BufferedReader br = new BufferedReader(isr);//junta os varios caracteres que estoa em uma linha.
+			
+			String readLine = br.readLine();
+			while(readLine!=null) {
+				System.out.println(readLine);
+				readLine=br.readLine();
+			}
+		
+			
+			br.close();
+			
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
