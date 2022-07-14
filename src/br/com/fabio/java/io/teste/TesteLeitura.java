0cda043fa22a5b362c00b5f638a4aca16c8099ef
FabioJRMoreira <tulkas.fabio@hotmail.com>
2022-07-14 14:13:11 -0400

    ajuste para as classes mae da hieraquia

--- src/br/com/fabio/java/io/teste/TesteLeitura.java
+++ src/br/com/fabio/java/io/teste/TesteLeitura.java
@@ -3,7 +3,9 @@ package br.com.fabio.java.io.teste;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class TesteLeitura {
 	public static void main(String[] args) {
@@ -12,18 +14,16 @@ public class TesteLeitura {
 			
 			//padrao de projeto decorator
 			//Fluxo de Entrada com arquivo
-			FileInputStream fis = new FileInputStream("lorem.txt");//binario
+			InputStream fis = new FileInputStream("lorem.txt");//binario
 			//ler um InputStream
-			InputStreamReader isr = new InputStreamReader(fis);//trasforma os bytes em caracteres
+			Reader isr = new InputStreamReader(fis);//trasforma os bytes em caracteres
 			BufferedReader br = new BufferedReader(isr);//junta os varios caracteres que estoa em uma linha.
-			
 			String readLine = br.readLine();
 			while(readLine!=null) {
 				System.out.println(readLine);
 				readLine=br.readLine();
 			}
-		
-			
+	
 			br.close();
 			
 		}catch (IOException e) {
